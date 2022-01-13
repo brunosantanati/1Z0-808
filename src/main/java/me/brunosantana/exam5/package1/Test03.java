1f6139a67f8811bf18e198bc867f9e2d7c47ae97
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2022-01-13 14:06:27 -0300

    Add more code examples from fifth test simulation.

---
+++ src/main/java/me/brunosantana/exam5/package1/Test03.java
@@ -0,0 +1,25 @@
+package me.brunosantana.exam5.package1;
+
+public class Test03 {
+	
+	public static final void main(String... a) { //OK
+	    System.out.println("Java Rocks!");
+	}
+	
+	/*public void main(String... args) { // IT DOES NOT EXECUTE
+	    System.out.println("Java Rocks!");
+	}
+	
+	static void main(String [] args) { // IT DOES NOT EXECUTE
+	    System.out.println("Java Rocks!");
+	}
+	
+	public static void main(String [] args) { //OK
+	    System.out.println("Java Rocks!");
+	}
+	
+	public static void main(String args) { // IT DOES NOT EXECUTE
+	    System.out.println("Java Rocks!");
+	}*/
+
+}
---
+++ src/main/java/me/brunosantana/exam5/package1/Test04.java
@@ -0,0 +1,15 @@
+package me.brunosantana.exam5.package1;
+
+public class Test04 {
+    public static void main(String[] args) {
+        String [][] arr = { {"%", "$$"}, {"***", "@@@@", "#####"}};
+        for(String [] str : arr) {
+            for(String s : str) {
+                System.out.println(s);
+                if(s.length() == 4) //Line n1
+                    break; //Line n2
+            }
+            break; //Line n3
+        }
+    }
+}
